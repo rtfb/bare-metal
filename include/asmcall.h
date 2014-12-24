6744f548a5c51bec023a857a81c174adc52da2bb
Vytautas Šaltenis <vytas@rtfb.lt>
2014-12-24 20:38:56 +0200

    Relocate function and call it in the new location

---
+++ include/asmcall.h
@@ -0,0 +1,7 @@
+#ifndef ASMCALL_H
+#define ASMCALL_H
+
+extern void BRANCHTO(unsigned int);
+extern uint32_t CALL_1(uint32_t addr, char const* param);
+
+#endif
--- main.c
+++ main.c
@@ -7,12 +7,21 @@
 #include "b64.h"
 #include "strutil.h"
 #include "debug.h"
+#include "asmcall.h"
 
 #define UNUSED(x) (void)(x)
 
 const char halting[] = "\r\n*** system halting ***";
 const char ready[] = "ready\r\n";
 const char yoo[] = "yoo!";
+
+void mem_cpy(uint32_t from, uint32_t to, uint32_t len) {
+    uint8_t *p_from = (uint8_t*) from;
+    uint8_t *p_to = (uint8_t*) to;
+    for (int i = 0; i < len; ++i) {
+        *p_to++ = *p_from++;
+    }
+}
 
 // kernel main function, it all begins here
 void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags) {
@@ -43,6 +52,12 @@ void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags) {
                 inspect_memory(buff+2);
             } else if (str_startswith(buff, "icky")) {
                 uart_puts(yoo);
+            } else if (str_startswith(buff, "freloc")) {
+                char tmp[32];
+                uint32_t tmplen = ARR_LEN(tmp);
+                uint32_t func_len = ((uint32_t) str_parse_int) - ((uint32_t) str_len);
+                mem_cpy((uint32_t)str_len, 0x30000, func_len);
+                uart_puts(str_int_to_str(CALL_1(0x30000, "xyz"), tmp, tmplen));
             } else {
                 int strlen = str_len(buff) - 1;
                 int j = 0;
